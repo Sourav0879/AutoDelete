2e8cb68b39dc48becbe1e85233bc68e3c5caf137
Sourav0879 <145244763+Sourav0879@users.noreply.github.com>
2025-02-25 17:52:39 +0600

    Update info.py

--- utils/info.py
+++ utils/info.py
@@ -18,8 +18,8 @@
 
 import os
 
-API_ID       = int(os.environ.get("API_ID", "26083023"))
-API_HASH     = os.environ.get("API_HASH", "4dd1e00d9d977f74435a83927273feba")
+API_ID       = int(os.environ.get("API_ID", "21100083"))
+API_HASH     = os.environ.get("API_HASH", "8980ecdc34880454c2e31eec10033f5b")
 BOT_TOKEN    = os.environ.get("BOT_TOKEN", "7022937720:AAGmoo7EYLqnOc7oDQlvF-xMCO6Mchq2NNg")
 SESSION      = os.environ.get("SESSION", "BQFB9jMARMpSHGH6U3u-bWlx2K3CvNHs4rc_j0O2Mz5WrXAOH_ilBAqY8rlPTDMH-k9dHx4ipKcKRmhSVBpM2z5ZVp7cim2KiYkeC02FFg_mVgvLpSYSPmRlNNujS3lOZ3KsHHoA6PVpyfeKXIpY9PXNrXQzBEiLILMZeqTF2eSJmTCK6dwuvhVF06TyH88lRi7jKvnWwSXS70Fx42vEhaaDy2u7uHuD59G6IJsz2ZPfq9K8vURVbuhhDi84PJIUXkY5M_UGEVYTNJYlE36M1yXNN_GCICF-86459VKUkFMiTJIF7W5WGXIb99EBFRxscZSHx2smjDp2mmqgvtRU3N_iFWTuyAAAAAHEqf9yAA")
 TIME         = int(os.environ.get("TIME", 10))
